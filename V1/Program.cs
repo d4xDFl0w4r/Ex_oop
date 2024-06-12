7c3a0336789a0d48f8f4639f43a2d8a534c91137
d4xDFl0w4r <d4xdfl0w4r@yandex.ru>
2024-06-12 17:19:09 +0300

    V1

---
+++ V1/Program.cs
@@ -0,0 +1,58 @@
+﻿abstract class Figure
+{
+    public virtual double Area()
+    {
+        return 0;
+    }
+}
+
+class Rectangle(double a, double b) : Figure
+{
+    public override double Area()
+    {
+        return a * b;
+    }
+}
+
+class Circle(double r) : Figure
+{
+    public override double Area()
+    {
+        return Math.PI * r * r;
+    }
+}
+
+class RightTriangle(double a, double b) : Figure
+{
+    public override double Area()
+    {
+        return 0.5 * a * b;
+    }
+}
+
+class Trapezoid(double a, double b, double h) : Figure
+{
+    public override double Area()
+    {
+        return 0.5 * (a + b) * h;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var figures = new Figure[]
+        {
+            new Rectangle(2, 3),
+            new Circle(4),
+            new RightTriangle(3, 4),
+            new Trapezoid(4, 3, 6)
+        };
+
+        foreach (var figure in figures)
+        {
+            Console.WriteLine(figure.Area());
+        }
+    }
+}
