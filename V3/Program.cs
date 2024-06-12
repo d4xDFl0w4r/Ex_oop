35e631af32635ed6e90d0f946996ba51f5adcc8a
d4xDFl0w4r <d4xdfl0w4r@yandex.ru>
2024-06-12 17:58:23 +0300

    V3

--- V3/Program.cs
+++ V3/Program.cs
@@ -1,19 +1,48 @@
-﻿namespace V3;
+﻿using System;
+
+abstract class Curve
+{
+    public abstract double CalculateY(double x);
+}
+
+class Line(double a, double b) : Curve
+{
+    public override double CalculateY(double x)
+    {
+        return a * x + b;
+    }
+}
+
+class Ellipse(double a, double b) : Curve
+{
+    public override double CalculateY(double x)
+    {
+        return Math.Sqrt(1 - (x * x) / (a * a)) * b;
+    }
+}
+
+class Hyperbola(double a, double b) : Curve
+{
+    public override double CalculateY(double x)
+    {
+        return Math.Sqrt(x * x / (a * a) - 1) * b;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var curves = new Curve[]
+        {
+            new Line(2, 3),
+            new Ellipse(2, 3),
+            new Hyperbola(2, 3)
+        };
+
+        foreach (var curve in curves)
+        {
+            Console.WriteLine("y: " + curve.CalculateY(2));
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
